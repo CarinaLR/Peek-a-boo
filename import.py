b605c44cd74eb2a9dd519c7dbb806b794ae140c5
CarinaLR <carinalopez@Carinas-MBP.home>
2020-06-07 16:58:05 -0400

    add tables

--- import.py
+++ import.py
@@ -4,18 +4,24 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
 def main():
+    # Create tables based on each table definition in `models`
+    db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE reviews (id INTEGER PRIMARY KEY, user_id INTEGER,book_isbn VARCHAR, CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id), CONSTRAINT book_isbn_fk FOREIGN KEY (book_isbn) REFERENCES books (isbn), comment VARCHAR, rating INTEGER NOT NULL, date TIMESTAMP NOT NULL)")
     f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                    {"isbn": isbn, "title": title, "author": author, "year": year})
         print(f"Added book {isbn} , {title} by {author} published on {year}.")
+        print("done")
     db.commit()
 
 
--- models.py
+++ models.py
@@ -9,27 +9,26 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
     _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, db.Integer, nullable=False)
+    name = db.Column(db.Varchar, nullable=False)
+    email = db.Column(db.Varchar, nullable=False)
+    password = db.Column(db.Varchar, nullable=False)
 
 
 class Book(db.Model):
     __tablename__ = "books"
-    _id = db.Column("id", db.Integer, primary_key=True)
-    isbn = db.Column("ISBN", db.Integer, nullable=False)
-    title = db.Column("title", db.String)
-    author = db.Column("author", db.String)
-    year = db.Column("year", db.Integer)
+    isbn = db.Column("isbn", db.Varchar, primary_key=True)
+    title = db.Column("title", db.Varchar, nullable=False)
+    author = db.Column("author", db.Varchar, nullable=False)
+    year = db.Column("year", db.Varchar, nullable=False)
 
 
 class Review(db.Model):
     __tablename__ = "reviews"
     _id = db.Column("id", db.Integer, primary_key=True)
     user_id = db.Column("user_id", db.Integer,
-                        db.ForeignKey("user.id"), nullable=False)
-    book_id = db.Column("book_id", db.Integer,
-                        db.ForeignKey("book.id"), nullable=False)
-    comment = db.Column("comment", db.Text)
-    rating = db.Column("rating", db.Integer)
+                        db.ForeignKey("user.id"))
+    book_isbn = db.Column("book_isbn", db.Integer,
+                          db.ForeignKey("book.isbn"))
+    comment = db.Column("comment", db.Varchar)
+    rating = db.Column("rating", db.Integer, nullable=False)
     date = db.Column("date", db.Timestamp)
