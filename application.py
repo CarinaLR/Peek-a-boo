1c2cc7e29384aede2ed39d5d291e01a2af7c4981
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-06-08 11:43:27 -0400

    registration page working

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 
-from flask import Flask, session, render_template, request, flash
+from flask import Flask, session, render_template, redirect, request, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -56,23 +56,71 @@ def goodreads():
 
 @app.route("/sign-up", methods=["POST", "GET"])
 def signup():
+    # Set variables.
     headline = "Please enter your information below."
-    email = None
-    if "signup" in session:
-        signup = session["signup"]
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
 
-        if request.method == "POST":
-            email = request.form["email"]
-            session["email"] = email
-            flash("Email was saved!")
+    # Clear session.
+    session.clear()
+
+    # Submitting a form via POST
+    if request.method == "POST":
+
+        # Check if info has an username
+        if not request.form.get("username"):
+            return render_template("register.html", message="must be register")
+
+        # Access database for username
+        userCheck = db.execute("SELECT * FROM users WHERE name = :username",
+                               {"username": request.form.get("username")}).fetchone()
+
+        # Check if username already exist
+        if userCheck:
+            return render_template("login.html", message="username already exist")
+
+        # Check if info has a password
+        elif not request.form.get("password"):
+            return render_template("login.html", message="must be login")
+
+        # Check if info has an email
+        elif not request.form.get("email"):
+            # Access database for email
+            userEmail = db.execute("SELECT * FROM users WHERE email = :email",
+                                   {"email": request.form.get("email")}).fetchone()
+            if userEmail:
+                return render_template("login.html", message="email already exist")
+
+            return render_template("login.html", message="must be login")
+
+        # Check passwords are equal
+        elif not request.form.get("password") == request.form.get("password"):
+            return render_template("register.html", message="passwords didn't match")
+
+        # Insert info into database
+        db.execute("INSERT INTO users (id, name, email, password) VALUES (:id,:username, :email, :password)",
+                   {"id": 1,
+                    "username": request.form.get("username"),
+                    "email": request.form.get("email"),
+                    "password": request.form.get("password")})
+
+        # Commit changes to database
+        db.commit()
+
+        flash('Account created', 'info')
+
+        # Redirect user to login page
+        return redirect("/login")
+
+    # User reached route via redirect from Search botton.
     else:
-        if "email" in session:
-            email = session["email"]
-    return render_template("register.html", headline=headline, email=email)
+        return render_template("register.html", headline=headline)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    # Set variables.
     headline = "Welcome, you are not logged in!"
     return render_template("login.html", headline=headline)
 
@@ -85,7 +133,7 @@ def logout():
     return render_template("logout.html", headline=headline)
 
 
-@app.route("/request")
-def request():
+@app.route("/book-info")
+def bookinfo():
     headline = "Enter the information requested to help you with your search."
-    return render_template("request.html", headline=headline)
+    return render_template("bookinfo.html", headline=headline)
